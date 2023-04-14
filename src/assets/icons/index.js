6aa1f7cccda722f52c52f6269b1ce1810e6d266b
jogopin <jonn.g.pineda@gmail.com>
2023-04-14 19:54:01 +0200

    install dependencies & add icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,6 @@
+import cartIcon from "./cartIcon.png"
+import searchIcon from "./searchIcon.svg";
+import logo from "./logo.png";
+import menu from "./menu.png"
+
+export {cartIcon, searchIcon, logo, menu} 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor:{
+        "blueGray":"#324d67",
+      }
+    },
   },
   plugins: [],
 }
