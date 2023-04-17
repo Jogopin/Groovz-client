544fb95f2b114d76797fcd6f4d35c31bf6dba5c6
jogopin <jonn.g.pineda@gmail.com>
2023-04-17 14:29:47 +0200

    Fix MenuModal display for larger screens

--- src/components/MenuModal.jsx
+++ src/components/MenuModal.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const MenuModal = ({ isMenuModalOn }) => {
 
-    if(isMenuModalOn) return(<></>)
+    if(!isMenuModalOn) return(<></>)
     
 
 
   return (
-    <nav className="fixed flex flex-col justify-between right-0 top-0 z-40 h-[500px] w-[330px] rounded-md bg-black pt-16 pb-10 text-white">
+    <nav className="fixed lg:hidden flex flex-col justify-between right-0 top-0 z-40 h-[500px] w-[330px] rounded-md bg-black pt-16 pb-10 text-white">
     {/* Hidden NavLinks */}
     <ul className="flex w-full flex-col items-center gap-12">
       <li className="w-[60%] pb-6 text-center">
