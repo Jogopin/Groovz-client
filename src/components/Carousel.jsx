e3ce4736d6d548ff97a87d057d60cff92e27233c
jogopin <jonn.g.pineda@gmail.com>
2023-04-24 12:59:27 +0200

    Improve Carousel responsiveness & style

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -21,28 +21,29 @@ const Carousel = ({ imagesList }) => {
   }
 
   return (
-    <div className="group relative m-auto  h-[780px] max-w-[1400px] border-2 px-4 py-4">
+    <div className="group relative h-96 w-full  m-auto aspect-square">
       <div
         style={{ backgroundImage: `url(${imagesList[currentIndex]})` }}
-        className="h-full w-full rounded-lg border-2 bg-contain bg-center bg-no-repeat duration-500"
+        className="h-full w-full rounded-lg bg-zinc-300  bg-contain bg-center bg-no-repeat duration-500"
       ></div>
       {/* left arrow */}
       <div
         onClick={prevSlide}
-        className="absolute left-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block"
+        className="absolute left-5 top-[50%] lg:hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-zinc-400 p-2 text-2xl lg:group-hover:block"
       >
         <img className="h-8 w-8" src={lessThanIcon} />
       </div>
       {/* right arrow */}
       <div
         onClick={nextSlide}
-        className="absolute right-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block"
+        className="absolute right-5 top-[50%] lg:hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl lg:group-hover:block"
       >
         <img className="h-8 w-8" src={moreThanIcon} />
       </div>
-      <div className="top-4 flex justify-center border-2 py-2">
+      {/* small imgs */}
+      <div className="top-4 hidden md:flex justify-center  py-2">
         {imagesList.map((img, imgIndex) => (
-          <div className="border-2 cursor-pointer" key={imgIndex} onClick={()=>{goToImage(imgIndex)}}>
+          <div  className={`${imgIndex===currentIndex ? `bg-red-700`: `bg-zinc-300`} m-2 cursor-pointer rounded-md `} key={imgIndex} onClick={()=>{goToImage(imgIndex)}}>
             <img className="w-20" src={img} />
           </div>
         ))}
