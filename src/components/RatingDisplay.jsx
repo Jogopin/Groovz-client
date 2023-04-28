4a7311a3a545a9d6e5c5715d3a7a9ce775ae381e
jogopin <jonn.g.pineda@gmail.com>
2023-04-28 18:22:18 +0200

    Refactor RatingDisplay component

--- src/components/RatingDisplay.jsx
+++ src/components/RatingDisplay.jsx
@@ -1,37 +1,18 @@
 import React from "react";
+import Star from "./Star";
+import generateStarsArray from "../utils/generateStarsArray";
 
 
 
 const RatingDisplay = ({starAverage=2.5}) => {
-  const fullStars = Math.floor(starAverage);
-  const starArr = [];
 
-  for (let i = 1; i <= fullStars; i++) {
-    starArr.push(1);
-  }
-  if (starAverage < 5) {
-    const partialStar = starAverage - fullStars;
-    starArr.push(partialStar);
-    const emptyStars = 5 - starArr.length;
-    for (let i = 1; i <= emptyStars; i++) {
-      starArr.push(0);
-    }
-  }
+  
+  const starArr = generateStarsArray(starAverage)
 
   return (
     <div className="flex gap-0.5">
-      {starArr.map((val, i) => (
-        <div
-          className="w-6 h-6  rounded-md text-xl text-white flex items-center justify-center cursor-default"
-          key={i}
-          style={{
-            background: `linear-gradient(90deg, #B91C1C ${
-              val * 100
-            }%, #CBD5E1 ${val * 100}%)`,
-          }}
-        >
-          ★
-        </div>
+      {starArr.map((starValue, i) => (
+        <Star key={i} starValue={starValue}/>
       ))}
       <span className="ml-2 font-semibold">{starAverage}</span>
     </div>
