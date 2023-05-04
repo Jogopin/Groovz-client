badfafff933c621bbb3ba8fc9b5d7a6ebec6f050
jogopin <jonn.g.pineda@gmail.com>
2023-05-04 20:44:11 +0200

    Add Login & Signup routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route,  Routes } from "react-router-dom"
 import { Footer, Navbar } from "./components"
 import Home from "./pages/Home"
 import Store from "./pages/Store"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
 
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" element={<Home/>}/>
         {/* STORE */}
         <Route path="/store/*" element={<Store/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
       
       <Footer/>
