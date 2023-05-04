4b6cfa8789746447396643d1a305e925733c855d
jogopin <jonn.g.pineda@gmail.com>
2023-05-04 20:52:25 +0200

    Add Product Page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductDetails from '../components/ProductDetails'
+import RatingReviewInput from '../components/RatingReviewInput'
+
+const Product = () => {
+  return (
+    <>
+        <ProductDetails/>
+        <RatingReviewInput/>
+    </>
+  )
+}
+
+export default Product
