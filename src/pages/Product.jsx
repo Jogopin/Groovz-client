55c35c0968f703f5270f151765770104e31f2aea
jogopin <jonn.g.pineda@gmail.com>
2023-05-04 22:03:29 +0200

    Updated Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ProductDetails from '../components/ProductDetails'
 import RatingReviewInput from '../components/RatingReviewInput'
+import { useParams } from 'react-router-dom'
 
 const Product = () => {
+  const {productId} = useParams()
+  
   return (
     <>
         <ProductDetails/>
