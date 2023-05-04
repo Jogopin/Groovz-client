dc7ce206f151cdd899a916d4c76b515ad33c6a6b
jogopin <jonn.g.pineda@gmail.com>
2023-05-04 20:31:23 +0200

    Add inputLabelText component

---
+++ src/components/InputLabelText.jsx
@@ -0,0 +1,22 @@
+export default function InputLabelText({
+  placeholder,
+  label,
+  id,
+  name,
+  inputRef,
+}) {
+  return (
+    <div className="flex items-center">
+      <label className=" w-32 font-semibold" htmlFor={id}>
+        {label}{" "}
+      </label>
+      <input
+        ref={inputRef}
+        placeholder={placeholder}
+        className="w-52 rounded-md border-2 border-zinc-800 px-2 py-1"
+        id={id}
+        name={name}
+      />
+    </div>
+  );
+}
