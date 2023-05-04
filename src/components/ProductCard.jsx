61de39fa8ecc0e59d6718b5fc73169ee7652f26d
jogopin <jonn.g.pineda@gmail.com>
2023-05-04 22:03:05 +0200

    Add Link to Product Page in ProductCard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { addCartIcon } from "../assets/icons";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({product}) => {
 
 
   return (
     <div className="flex flex-col items-center ">
-      <div
+      <Link
+      to={`/store/${product._id}`}
         // key={product._id}
         className="flex relative aspect-square items-center justify-center rounded-lg bg-zinc-300 duration-300 ease-in-out hover:scale-105 hover:drop-shadow-2xl"
       >
         <img className="h-full" src={product.images[0]} />     
-      </div>
+      </Link>
       <div className="flex w-full items-center justify-between text-zinc-800">
         <button className="ease-in h-12 w-12 md:h-8 md:w-8 duration-200 hover:scale-125">
           <img src={addCartIcon} />
