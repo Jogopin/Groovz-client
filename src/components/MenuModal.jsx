bab8a7c65b2f9be1774c4485e494df9d70b46811
jogopin <jonn.g.pineda@gmail.com>
2023-06-27 11:15:46 +0200

    Modify MenuModal to reflect user login status

--- src/components/MenuModal.jsx
+++ src/components/MenuModal.jsx
@@ -1,38 +1,86 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { AuthContext } from '../context/auth.context'
 
 const MenuModal = ({ isMenuModalOn, toggleMenuModal }) => {
 
+    const {isLoggedIn,user,logOutUser} = useContext(AuthContext)
     if(!isMenuModalOn) return(<></>)
     
 
 
   return (
-    <nav className="fixed lg:hidden flex flex-col justify-between right-0 top-0 z-40 h-[500px] w-[330px] rounded-md bg-zinc-800 pt-16 pb-10 text-white">
-    {/* Hidden NavLinks */}
-    <ul className="flex w-full flex-col items-center gap-12">
-      <li className="w-[60%] pb-6 text-center">
-        <NavLink className="px-1 pb-1" to={"/store"} onClick={toggleMenuModal}>
-          Store
-        </NavLink>
-      </li>
-      <li className="w-[60%] pb-6 text-center">
-        <NavLink className="px-1 pb-1" to={"/about-us"} onClick={toggleMenuModal}>
-          About Us
-        </NavLink>
-      </li>
-    </ul>
+    <nav className="fixed right-0 top-0 z-40 flex h-[500px] w-[330px] flex-col justify-between rounded-md bg-zinc-800 pb-10 pt-16 text-white lg:hidden">
+      {/* Hidden NavLinks */}
+      <ul className="flex w-full flex-col items-center gap-12">
+        <li className="w-[60%] pb-6 text-center">
+          <NavLink
+            className="px-1 pb-1"
+            to={"/store"}
+            onClick={toggleMenuModal}
+          >
+            Store
+          </NavLink>
+        </li>
+        <li className="w-[60%] pb-6 text-center">
+          <NavLink
+            className="px-1 pb-1"
+            to={"/about-us"}
+            onClick={toggleMenuModal}
+          >
+            About Us
+          </NavLink>
+        </li>
+      </ul>
       {/* Login Signup  profile Logout */}
-    <ul className="flex w-[70%] mx-auto items-center gap-12 border-t-2 pt-7 ">
-      <li className="w-[60%]  text-center">
-        <NavLink to={"/login"} className="px-1 pb-1" onClick={toggleMenuModal}>Login</NavLink>
-      </li>
-      <li className="w-[60%] text-center">
-        <NavLink to={"/signup"} className="px-1 pb-1"  onClick={toggleMenuModal}>Sign Up</NavLink>
-      </li>
-    </ul>
-  </nav>
-  )
+      {isLoggedIn ? (
+        <ul className="mx-auto flex w-[70%] items-center gap-12 border-t-2 pt-7 ">
+          <li className="w-[60%]  text-center">
+            <NavLink
+              to={"/profile"}
+              className="px-1 pb-1"
+              onClick={toggleMenuModal}
+            >
+              {user.username}
+            </NavLink>
+          </li>
+          <li className="w-[60%] text-center">
+            <button
+              
+              className="px-1 pb-1"
+              onClick={()=>{
+                logOutUser() 
+                toggleMenuModal()
+              }}
+            >
+              Log Out
+            </button>
+          </li>
+        </ul>
+      ) : (
+        <ul className="mx-auto flex w-[70%] items-center gap-12 border-t-2 pt-7 ">
+          <li className="w-[60%]  text-center">
+            <NavLink
+              to={"/login"}
+              className="px-1 pb-1"
+              onClick={toggleMenuModal}
+            >
+              Login
+            </NavLink>
+          </li>
+          <li className="w-[60%] text-center">
+            <NavLink
+              to={"/signup"}
+              className="px-1 pb-1"
+              onClick={toggleMenuModal}
+            >
+              Sign Up
+            </NavLink>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
 }
 
 export default MenuModal
