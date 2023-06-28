4434995f229c0d4bc7ee7ef28f022123017e4669
jogopin <jonn.g.pineda@gmail.com>
2023-06-28 08:57:38 +0200

    Add form reset function and axios post call in AddProduct

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import InputLabelText from "../components/InputLabelText";
+import axios from "axios";
 
 export default function AddProduct() {
   const name = useRef(null);
@@ -10,17 +11,59 @@ export default function AddProduct() {
   const stock = useRef(null);
   const category = useRef(null);
 
-  const handleSubmit = () => {};
-  const handleCancel = () => {};
+  const resetForm=()=>{
+    name.current.value = null
+    reference.current.value = null
+    description.current.value = null
+    price.current.value = null
+    discount.current.value  = null
+    stock.current.value = null
+    category.current.value = null
+  }
+ 
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const newProduct={
+         name : name.current.value,
+         reference : reference.current.value,
+         description : description.current.value,
+         price : price.current.value,
+         discount : discount.current.value ? discount.current.value : 0,
+         stock : stock.current.value,
+         category : category.current.value, 
+    }
+
+    
+
+    axios.post(`${import.meta.env.VITE_API_URL}/products`,newProduct)
+    .then(response=>{
+
+        console.log("new product created",response.data)
+        resetForm()
+    })
+    .catch(error=>{
+        console.log("error creating a product, ",error.response.data.message)      
+                
+      })
+  };
+  const handleCancel = (e) => {
+    e.preventDefault()
+    resetForm()
+  };
+
+ 
   return (
     <div className="flex">
-      <form className="mx-auto my-10 flex flex-col items-start gap-4 ">
+      <form onSubmit={handleSubmit} className="mx-auto my-10 flex flex-col items-start gap-4 ">
         <InputLabelText
           id="name"
           label="Name"
           name="name"
           inputRef={name}
           placeholder={"Product's name"}
+
         />
         <InputLabelText
           id="reference"
@@ -40,6 +83,7 @@ export default function AddProduct() {
             rows="4"
             cols="60"
             className="h-60 w-52 rounded-md border-2 border-zinc-800 p-2 md:h-auto md:w-auto"
+            required
           />
         </div>
         <div className="flex items-center">
@@ -66,6 +110,7 @@ export default function AddProduct() {
             placeholder="Price "
             className="w-24 rounded-md border-2 border-zinc-800 p-2"
             type="number"
+            required
           />
           <span className="px-2 font-semibold">€</span>
         </div>
@@ -79,6 +124,7 @@ export default function AddProduct() {
             placeholder="Units in stock"
             className="w-36   rounded-md border-2 border-zinc-800 p-2"
             type="number"
+            required
           />
           <span className="px-2 font-semibold">units</span>
         </div>
@@ -92,12 +138,13 @@ export default function AddProduct() {
             placeholder="Discount (%)"
             className="w-36   rounded-md border-2 border-zinc-800 p-2"
             type="number"
+        
           />
           <span className="px-2 font-semibold">%</span>
         </div>
         <div className="m-auto flex gap-4">
           <button
-            onClick={handleSubmit}
+            
             className="btn-primary m-auto px-4 py-2"
           >
             Add Product
