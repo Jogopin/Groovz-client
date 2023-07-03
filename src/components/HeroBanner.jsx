d64be32ec75d0a6a7eab731a87fbaf7eabd7fe59
jogopin <jonn.g.pineda@gmail.com>
2023-07-03 15:34:52 +0200

    update: Dynamic data rendering in HeroBanner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import defImg from "../assets/imgs/defImg.webp";
-const HeroBanner = () => {
+
+const HeroBanner = ({heroProduct}) => {
+   
+  if(!heroProduct){
+    
+    return <p>"Loading"</p>
+  }
   return (
     <section className="flex w-full flex-col  space-y-5 rounded-lg  bg-zinc-300 md:flex-row-reverse md:items-center md:justify-center">
       {/* IMAGE */}
       <div className="flex flex-col items-center ">
-        <img src={defImg} alt="headphones" className="z-20 mr-0" />
+        <img src={heroProduct.images[0]} alt="headphones" className="z-20 mr-0" />
         <p className="-mt-5 mb-8 hidden  font-bold text-blueGray sm:block">
           the best headphones in the market
         </p>
@@ -15,14 +20,14 @@ const HeroBanner = () => {
       {/* TEXT */}
       <div className="pb-10 pl-10 ">
         <span className="text-xl font-bold uppercase tracking-tight text-red-700">
-          Lorem ipsum
+          lorem ipsum
         </span>
         <h1 className="-ml-1 mb-2 text-7xl font-bold tracking-tight">
-          Dolor sit amet
+        {heroProduct.name}
         </h1>
         <h2 className="text-2xl font-bold tracking-wide">Luctus sit amet</h2>
 
-        <Link to="/product/ID">
+        <Link to={`/store/${heroProduct._id}`}>
           <button className="mt-5 rounded-[30px] bg-zinc-800 px-8 py-4 font-bold text-white hover:bg-red-700">
            SHOP
           </button>
