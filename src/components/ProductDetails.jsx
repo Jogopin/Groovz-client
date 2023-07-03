f616d561a42ab9be815b62f45abc3d7aceaa1b23
jogopin <jonn.g.pineda@gmail.com>
2023-07-03 14:41:55 +0200

    Fetch and display product details in Product component

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,32 +1,31 @@
 import React from 'react'
 import Carousel from './Carousel'
-import defImg from "../assets/imgs/defImg.webp"
-import defImgRed from "../assets/imgs/defImgRed.png"
 import { addCartIcon } from '../assets/icons'
 import RatingDisplay from './RatingDisplay'
 import RatingReviewInput from './RatingReviewInput'
 
-const imagesList = [defImg,defImgRed]
 
-const ProductDetails = () => {
 
+const ProductDetails = ({productData}) => {
+
+
+  if(productData===null){
+    console.log("loading productData")
+    return "loading"
+  }
   return (
     <div className="flex flex-col lg:flex-row lg:max-w-5xl mx-auto  mt-16">
       {/* Left */}
-      <Carousel imagesList={imagesList} />
+      <Carousel imagesList={productData.images} />
       {/* Right */}
       <div className="p-4 text-zinc-800 flex flex-col gap-2">
-        <h2 className="py-2 text-3xl font-semibold">Product name</h2>
+        <h2 className="py-2 text-3xl font-semibold">{productData.name}</h2>
         {/* rating */}
         <RatingDisplay starAverage={3.7}/>
 
         <h4 className="text-xl font-semibold">Details</h4>
-        <p>
-          product description Lorem ipsum product description Lorem ipsum
-          product description Lorem ipsum product description Lorem ipsumproduct
-          description Lorem ipsum product description Lorem ipsum
-        </p>
-        <h2 className="text-3xl my-2 md:my-8 font-bold text-right text-red-700">€99.00</h2>
+        <p>{productData.description}</p>
+        <h2 className="text-3xl my-2 md:my-8 font-bold text-right text-red-700">{productData.price}€</h2>
         <div className="flex flex-col md:flex-row gap-4 w-3/4 my-2 items-center self-center lg:mt-8">
           <div className="flex h-14 w-3/4 items-center justify-around rounded-md bg-zinc-300 text-center">
             <button className="w-14 h-full text-lg font-bold text-red-700 hover:bg-zinc-400 hover:rounded-l-md">-</button>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductDetails from '../components/ProductDetails'
 import RatingReviewInput from '../components/RatingReviewInput'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const Product = () => {
   const {productId} = useParams()
+  const [productData, setProductData] = useState(null)
   
+
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_API_URL}/products/${productId}`)
+      .then(response=>setProductData(response.data))
+      .catch((error)=>{
+        console.log(`Error getting the product "${productId}", `,error)
+      })
+  },[productId])
+
   return (
     <>
-        <ProductDetails/>
+        <ProductDetails productData={productData} />
         <RatingReviewInput/>
     </>
   )
