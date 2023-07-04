79411ad3f7faf37364820bbccf427e1e53b78e35
jogopin <jonn.g.pineda@gmail.com>
2023-07-04 15:03:03 +0200

    Enhance layout and styling for ReviewsDisplay component

--- src/components/ReviewsDisplay.jsx
+++ src/components/ReviewsDisplay.jsx
@@ -25,12 +25,12 @@ export default function ReviewsDisplay ({productId}){
 
     return(
         
-        <div>
+        <div className="mx-auto flex flex-col sm:flex-row justify-center flex-wrap  lg:max-w-5xl mt-16 gap-5">
             {reviewsList.map(review=>(
-                <div key={review._id}>
-                    <p>{review.reviewText}</p>
+                <div className="md:w-1/4 px-2 flex flex-col   items-center gap-4" key={review._id}>
+                    <span className="font-semibold text-lg">{review.user.username}</span>
                     <RatingDisplay rating={review.rating}/>
-                    <span>{review.user.username}</span>
+                    <p className="line-clamp-4 text-zinc-500 text-center">{review.reviewText}</p>
                 </div>
             ))}
         </div>
