2f59f39f92c9ba69235c0120f60dd382a0fd0274
jogopin <jonn.g.pineda@gmail.com>
2023-07-05 11:36:47 +0200

    Make ReviewsDisplay receive reviewsList as prop

--- src/components/ReviewsDisplay.jsx
+++ src/components/ReviewsDisplay.jsx
@@ -2,22 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import RatingDisplay from "./RatingDisplay";
 
-export default function ReviewsDisplay ({productId}){
+export default function ReviewsDisplay ({reviewsList}){
 
-    const [ reviewsList, setReviewsList ]=useState(null)
-
-    useEffect(()=>{
-        axios.get(`${import.meta.env.VITE_API_URL}/reviews/${productId}`)
-        .then(response=>{
-            setReviewsList(response.data)
-            
-        })
-        .catch(error=>{
-            console.log("Error: getting the list of reviews",error)
-        })
-
-    },[productId])
-    console.log(reviewsList)
+    
+    
     
     if(!reviewsList){
         return "Loading"
