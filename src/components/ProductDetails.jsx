2cf3481a342f1c6066e2c9fde7339422eaa09c60
jogopin <jonn.g.pineda@gmail.com>
2023-07-07 15:06:41 +0200

    Add quantity control and add-to-cart functionality in ProductDetails

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Carousel from "./Carousel";
 import { addCartIcon } from "../assets/icons";
 import RatingDisplay from "./RatingDisplay";
+import { useCart } from "../hooks/useCart";
 
 const ProductDetails = ({ productData, rating }) => {
+  const {addToCart}=useCart()
+  const [quantity,setQuantity] =useState(1)
+
   if (productData === null) {
     console.log("loading productData");
     return "loading";
   }
+  const handleAddToCart =()=>{
+    if(quantity===0) return
+        
+    const productToCart = {...productData,quantity}
+    addToCart(productToCart)
+  }
+  const handleClickMinus =()=>{
+    if(quantity===0) return
+    setQuantity(prevState=>prevState-1)
+  }
+  const handleClickPlus =()=>{
+    if(quantity===5) return
+    setQuantity(prevState=>prevState+1)
+  }
+  
   return (
     <div className="mx-auto mt-16 flex flex-col justify-center lg:max-w-5xl  lg:flex-row ">
       {/* Left */}
@@ -37,17 +56,17 @@ const ProductDetails = ({ productData, rating }) => {
         <div className="my-2 flex  flex-col  items-center gap-4 self-center md:flex-row lg:mt-8 ">
           {/* Quantity control */}
           <div className="flex h-14 w-auto flex-none items-center justify-around rounded-md bg-zinc-300 text-center">
-            <button className="h-full w-14 text-lg font-bold text-red-700 hover:rounded-l-md hover:bg-zinc-400">
+            <button onClick={handleClickMinus} className="h-full w-14 text-lg font-bold text-red-700 hover:rounded-l-md hover:bg-zinc-400">
               -
             </button>
-            <span className="text-md w-14 font-bold">0</span>
-            <button className="h-full w-14 text-lg font-bold text-red-700 hover:rounded-r-md hover:bg-zinc-400">
+            <span className="text-md w-14 font-bold">{quantity}</span>
+            <button onClick={handleClickPlus} className="h-full w-14 text-lg font-bold text-red-700 hover:rounded-r-md hover:bg-zinc-400">
               +
             </button>
           </div>
           {/* Add to cart button */}
-          <button className="btn-primary flex h-14  items-center justify-evenly">
-            <img src={addCartIcon} className="w-8 invert" />
+          <button onClick={handleAddToCart} className="btn-primary flex h-14  items-center justify-evenly">
+            <img src={addCartIcon} className="w-8 invert" alt="Cart-Icon"/>
             <span>Add to the cart</span>
           </button>
         </div>
