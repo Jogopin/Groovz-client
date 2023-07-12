179780b4438a31d908f5c41a46eb95c224ef6eea
jogopin <jonn.g.pineda@gmail.com>
2023-07-12 17:12:45 +0200

    Add token management functions to api.js

--- src/services/api.js
+++ src/services/api.js
@@ -7,6 +7,13 @@ const privateAPI = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
 
+const setToken = token =>{
+    privateAPI.defaults.headers.common['Authorization']=`Bearer ${token}`
+}
+
+const removeToken = ()=>{
+    delete publicAPI.defaults.headers.common['Authorization'];
+}
 
 const errorHandler = (error)=>{
     throw error;
@@ -23,5 +30,9 @@ const uploadImage = async (file) => {
 
 
 export default{
+    publicAPI,
+    privateAPI,
+    setToken,
+    removeToken,
     uploadImage,
 }
