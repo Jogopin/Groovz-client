4095e2cd7b56a1ee435cfc5bab453f80de9d08d6
jogopin <jonn.g.pineda@gmail.com>
2023-07-18 12:26:37 +0200

    Refactor API calls to use a common handler function and add new API methods

--- src/services/api.js
+++ src/services/api.js
@@ -1,41 +1,42 @@
+// api.js
 import axios from "axios";
 
-export const publicAPI = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
-export const privateAPI = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+// create a public axios instance for making requests that don't require authentication
+const publicAPI = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
-export const setTokenInHeaders = token =>{
-    privateAPI.defaults.headers.common['Authorization']=`Bearer ${token}`
-}
+// create a private axios instance for making requests that require authentication
+const privateAPI = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
-export const clearTokenHeaders = ()=>{
-    delete privateAPI.defaults.headers.common['Authorization'];
-}
-
-export const errorHandler = (error)=>{
-    throw error;
-}
-
-export const uploadImage = async (file) => {
-    try {
-        const response = await publicAPI.post("/upload", file);
-        return response.data;
-    } catch (error) {
-        errorHandler(error);
-    }
+// function to set the Authorization token for the private axios instance
+export const setTokenInHeaders = (token) => {
+  privateAPI.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 };
-export const getProducts = async ()=>{
 
-    try{
-        const response = await publicAPI.get("/products")
-        return response.data
+// function to clear the Authorization token from the private axios instance
+export const clearTokenHeaders = () => {
+  delete privateAPI.defaults.headers.common["Authorization"];
+};
 
-    }catch(error){
-        errorHandler(error)
-    }
-}
+const callApi = async (apiMethod) => {
+  try {
+    const response = await apiMethod();
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-
+export const uploadImage = (file) => callApi(() => publicAPI.post("/upload", file));
+export const getProducts = () => callApi(() => publicAPI.get("/products"));
+export const getProductData = (productId) => callApi(() => publicAPI.get(`/products/${productId}`));
+export const getReviewsFromProduct = (productId) => callApi(() => publicAPI.get(`/reviews/${productId}`));
+export const postReview = (reviewObj) => callApi(() => privateAPI.post("/reviews", reviewObj));
+export const updateUserDetails = ({ userId, firstName, lastName, address }) => callApi(() => privateAPI.put(`/auth/user/${userId}`, { firstName, lastName, address }));
+export const getUserDetails = (userId) => callApi(() => privateAPI.get(`/auth/user/${userId}`));
+export const startCheckout = ({ productsToCheckout, customerData }) => callApi(() => publicAPI.post("/checkout", { productsToCheckout, customerData }));
+export const createProduct = (productData) => callApi(() => privateAPI.post("/products", productData));
