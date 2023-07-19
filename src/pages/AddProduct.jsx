4d24ac5b2a07ae7cc8801b6140c82471f649cd0c
jogopin <jonn.g.pineda@gmail.com>
2023-07-19 13:47:40 +0200

    Refactor AddProduct display & control components

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -3,24 +3,35 @@ import InputLabelText from "../components/InputLabelText";
 import { plusIcon } from "../assets/icons";
 import { uploadImage } from "../services/api";
 
-export default function AddProduct({addProductAndUpdateState}) {
-  const name = useRef(null);
-  const reference = useRef(null);
-  const description = useRef(null);
-  const price = useRef(null);
-  const discount = useRef(null);
-  const stock = useRef(null);
-  const category = useRef(null);
+export default function AddProduct({ addProductAndUpdateState }) {
+  const [formState, setFormState] = useState({
+    name: "",
+    reference: "",
+    description: "",
+    price: "",
+    discount: "",
+    stock: "",
+    category: "headphones",
+  });
   const [imageUrlList, setImageUrlsList] = useState([]);
 
+  const handleOnChange = (e) => {
+    setFormState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   const resetForm = () => {
-    name.current.value = null;
-    reference.current.value = null;
-    description.current.value = null;
-    price.current.value = null;
-    discount.current.value = null;
-    stock.current.value = null;
-    category.current.value = null;
+    setFormState({
+      name: "",
+      reference: "",
+      description: "",
+      price: "",
+      discount: "",
+      stock: "",
+      category: "",
+    });
     setImageUrlsList([]);
   };
 
@@ -30,14 +41,13 @@ export default function AddProduct({addProductAndUpdateState}) {
     const uploadData = new FormData();
 
     uploadData.append("imageUrl", file);
-    try{
-      const response = await uploadImage(uploadData)
+    try {
+      const response = await uploadImage(uploadData);
       setImageUrlsList((prevState) => [
         ...prevState,
         { url: response.fileUrl, name: file.name },
       ]);
-
-    }catch(error){
+    } catch (error) {
       console.log("Error while uploading the file: ", error);
     }
   };
@@ -46,21 +56,15 @@ export default function AddProduct({addProductAndUpdateState}) {
     e.preventDefault();
 
     const newProduct = {
-      name: name.current.value,
-      reference: reference.current.value,
-      description: description.current.value,
-      price: price.current.value,
-      discount: discount.current.value ? discount.current.value : 0,
-      stock: stock.current.value,
-      category: category.current.value,
-
+      ...formState,
       images: imageUrlList.map((item) => item.url),
     };
+    
 
-    try{
-      await addProductAndUpdateState(newProduct)
+    try {
+      await addProductAndUpdateState(newProduct);
       console.log("new product created");
-    }catch(error){
+    } catch (error) {
       console.log("error creating a product");
     }
   };
@@ -71,123 +75,150 @@ export default function AddProduct({addProductAndUpdateState}) {
   };
 
   return (
-    <div className="flex">
+    <div className="m-4">
       <form
         onSubmit={handleSubmit}
-        className="mx-auto my-10 flex flex-col items-start gap-4 "
+        className="mx-auto my-10 md:w-3/5 lg:w-2/5 flex flex-col items-center gap-4 "
       >
         <InputLabelText
           id="name"
           label="Name"
           name="name"
-          inputRef={name}
+          value={formState.name}
+          onChange={handleOnChange}
           placeholder={"Product's name"}
         />
         <InputLabelText
           id="reference"
           label="Reference"
           name="reference"
-          inputRef={reference}
+          value={formState.reference}
+          onChange={handleOnChange}
           placeholder={"Unique Reference"}
         />
-        <div className="flex items-center">
-          <label className=" w-32 font-semibold " htmlFor="description">
+        <div className="m-2 w-full">
+          <label
+            className="block text-xs font-medium text-gray-700"
+            htmlFor="description"
+          >
             Description
           </label>
           <textarea
             id="description"
-            ref={description}
+            name="description"
+            value={formState.description}
+            onChange={handleOnChange}
             placeholder="Description"
             rows="4"
-            cols="60"
-            className="h-60 w-52 rounded-md border-2 border-zinc-800 p-2 md:h-auto md:w-auto"
+            cols="50"
+            className="mt-1 h-60 rounded-md border-2 border-zinc-300 px-2 sm:h-full w-full"
             required
           />
         </div>
-        <div className="flex items-center">
-          <label className=" w-32 font-semibold " htmlFor="category">
-            Category
-          </label>
-          <select
-            id="category"
-            ref={category}
-            className="w-auto rounded-md border-2 border-zinc-800 p-2 "
-          >
-            <option value="headphones">Headphones</option>
-            <option value="speakers">Speakers</option>
-          </select>
-        </div>
-
-        <div className="flex items-center">
-          <label className=" w-32 font-semibold " htmlFor="price">
-            Price
-          </label>
-          <input
-            id="price"
-            ref={price}
-            placeholder="Price "
-            className="w-24 rounded-md border-2 border-zinc-800 p-2"
-            type="number"
-            step="0.01"
-            required
-          />
-          <span className="px-2 font-semibold">€</span>
-        </div>
-        <div className="flex items-center">
-          <label className=" w-32 font-semibold " htmlFor="stock">
-            Stock
-          </label>
-          <input
-            id="stock"
-            ref={stock}
-            placeholder="Units in stock"
-            className="w-36   rounded-md border-2 border-zinc-800 p-2"
-            type="number"
-            required
-          />
-          <span className="px-2 font-semibold">units</span>
-        </div>
-        <div className="flex items-center">
-          <label className=" w-32 font-semibold " htmlFor="discount">
-            Discount
-          </label>
-          <input
-            id="discount"
-            ref={discount}
-            placeholder="Discount (%)"
-            className="w-36   rounded-md border-2 border-zinc-800 p-2"
-            type="number"
-          />
-          <span className="px-2 font-semibold">%</span>
-        </div>
-
-        <div className="flex w-full flex-wrap justify-center gap-2 rounded-xl border-2 border-black bg-zinc-100 p-2 shadow-inner">
-          <div>
-            <label className="flex h-32 w-32 items-center justify-center rounded-2xl  bg-zinc-300 duration-300 ease-in-out hover:scale-105 hover:drop-shadow-lg">
-              <input type="file" onChange={handleFileUpload} hidden />
-              <img className="h-max" src={plusIcon} />
+        <div className="flex flex-row">
+        {/* Price & Discount */}
+        <div className="mx-auto flex flex-col">
+          <div className="m-2 w-full">
+            <label
+              className="block text-xs font-medium text-gray-700"
+              htmlFor="price"
+            >
+              Price
             </label>
-            <p className="w-full truncate text-center font-semibold">
-              Add Image
-            </p>
+            <input
+              id="price"
+              name="price"
+              onChange={handleOnChange}
+              placeholder="Price "
+              className="mt-1 w-40 rounded-md border-2 border-zinc-300 px-2 sm:text-sm"
+              type="number"
+              step="0.01"
+              required
+            />
+            <span className="px-1 font-medium text-gray-700">€</span>
           </div>
+          <div className="m-2 w-full ">
+            <label
+              className="block text-xs font-medium text-gray-700"
+              htmlFor="discount"
+            >
+              Discount
+            </label>
+            <input
+              id="discount"
+              name="discount"
+              onChange={handleOnChange}
+              placeholder="Discount (%)"
+              className="mt-1 w-40 rounded-md border-2 border-zinc-300 px-2 sm:text-sm"
+              type="number"
+            />
+            <span className="px-1 font-medium text-gray-700">%</span>
+          </div>
+        </div>
+        
+        {/* Stock & category */}
+        <div className="mx-auto flex flex-col">
+          <div className="m-2 w-full">
+            <label
+              className="block text-xs font-medium text-gray-700"
+              htmlFor="stock"
+            >
+              Stock
+            </label>
+            <input
+              name="stock"
+              id="stock"
+              onChange={handleOnChange}
+              placeholder="Units in stock"
+              className="mt-1 w-40 rounded-md border-2 border-zinc-300 px-2 sm:text-sm"
+              type="number"
+              required
+            />
+            <span className="px-1 font-medium text-gray-700">u</span>
+          </div>
+          <div className="m-2 w-full">
+            <label
+              className="block text-xs font-medium text-gray-700"
+              htmlFor="category"
+            >
+              Category
+            </label>
+            <select
+              id="category"
+              name="category"
+              onChange={handleOnChange}
+              className="mt-1 w-40 rounded-md border-2 border-zinc-300 px-2 sm:text-sm"
+            >
+              <option value="headphones">Headphones</option>
+              <option value="speakers">Speakers</option>
+            </select>
+          </div>
+        </div>
+        </div>
 
-          {imageUrlList.map((item, i) => (
-            <div className="w-32" key={i}>
-              <img className="w-full" src={item.url} alt="uploaded" />
-              <p className="w-full truncate text-center font-semibold ">
-                {item.name}
-              </p>
+        <div className="m-2 w-full">
+          <p className="block text-xs font-medium text-gray-700">Add Image</p>
+          <div className="mt-1 flex flex-wrap justify-center gap-2 rounded-xl border-2 border-zinc-300 bg-zinc-100  p-2 shadow-inner sm:w-full">
+            <div className="w-32 ">
+              <label className="flex h-32  items-center justify-center rounded-2xl  bg-zinc-300 duration-300 ease-in-out hover:scale-105 hover:drop-shadow-lg">
+                <input type="file" onChange={handleFileUpload} hidden />
+                <img className="h-max" src={plusIcon} />
+              </label>
             </div>
-          ))}
+            {imageUrlList.map((item, i) => (
+              <div className="w-32" key={item.url}>
+                <img className="w-full" src={item.url} alt="picture uploaded" />
+                <p className="w-full truncate text-center font-semibold ">
+                  {item.name}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
 
         <div className="m-auto flex gap-4">
-          <button className="btn-primary m-auto px-4 py-2">Add Product</button>
-          <button
-            onClick={handleCancel}
-            className="btn-primary m-auto px-4 py-2"
-          >
+          <button className="btn-primary m-auto">Add Product</button>
+          <button onClick={handleCancel} className="btn-primary m-auto">
             Cancel
           </button>
         </div>
