d03a21c7066cbbabac841fd8db0d73964c6c3a05
jogopin <jonn.g.pineda@gmail.com>
2023-07-19 15:25:27 +0200

    Refactor Signup component Replace useRef with useState for username, password, and email state management Update handleSubmit function to use state values instead of refs Modify InputLabelText components to use state values and onChange handlers Add styling to the form for better layout and readability Improve the UX

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import InputLabelText from "../components/InputLabelText";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
@@ -8,18 +8,18 @@ const Signup = () => {
 
   const navigate = useNavigate()
   
-  const username = useRef(null)
-  const email  = useRef(null)
-  const password = useRef(null)
+  const [username,setUsername] = useState("")
+  const [password,setPassword] = useState("")
+  const [email,setEmail] = useState("")
   const [errorMessage, setErrorMessage] = useState(null)
 
   const handleSubmit = (e)=>{
     e.preventDefault()
 
     const newUser ={
-      username:username.current.value,
-      email: email.current.value,
-      password:password.current.value
+      username,
+      email,
+      password
     }
 
     axios.post(`${import.meta.env.VITE_API_URL}/auth/signup`,newUser)
@@ -35,12 +35,13 @@ const Signup = () => {
   }
   return (
     <>
-      <form onSubmit={handleSubmit} className="m-8 flex flex-col items-center gap-8">
-        <InputLabelText
+      <form onSubmit={handleSubmit} className="mx-auto my-10 w-4/6 sm:w-2/5 lg:w-1/5 flex flex-col items-center gap-4 ">
+      <InputLabelText
           id="username"
           label="Username"
           name="username"
-          inputRef={username}
+          value={username}
+          onChange={(e)=>{setUsername(e.target.value)}}
           placeholder="your username"
           type={"text"}
         />
@@ -48,7 +49,8 @@ const Signup = () => {
           id="password"
           label="Password"
           name="password"
-          inputRef={password}
+          value={password}
+          onChange={(e)=>{setPassword(e.target.value)}}
           placeholder="your password"
           type={"password"}
         />
@@ -56,18 +58,22 @@ const Signup = () => {
           id="email"
           label="Email"
           name="email"
-          inputRef={email}
+          value={email}
+          onChange={(e)=>{setEmail(e.target.value)}}
           placeholder="your email"
           type={"email"}
         />
+        <p className="text-sm text-gray-500">
+        {"Already have account? "}
+        <Link to={"/login"} className="font-bold underline">Login</Link> 
+        </p>
         <button  className="btn-primary px-4 py-2">
           Sign up
         </button>
       </form>
 
       {errorMessage ? <p>{errorMessage}</p> : <></>}
-      <p>Already have account?</p>
-      <Link to={"/login"}> Login</Link>
+      
     </>
   );
 };
