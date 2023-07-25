cc001d181ce344da3d2e447e355b3a47edeaef29
jogopin <jonn.g.pineda@gmail.com>
2023-07-25 21:30:29 +0200

    Add date formatting and list sorting to UserOrders

--- src/pages/ProfilePage/UserOrders.jsx
+++ src/pages/ProfilePage/UserOrders.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getOrdersFromUser } from "../../services/api"
+import dayjs from "dayjs"
 
 export default function UserOrders ({userId}){
     const [ordersList,setOrdersList] =useState([])
@@ -12,6 +13,7 @@ export default function UserOrders ({userId}){
           try{
             const response = await getOrdersFromUser(userId)
             console.log(response)
+            response.sort((a,b)=>dayjs(b.createdAt) - dayjs(a.createdAt))
             setOrdersList(response)
           }catch(error){
             console.error(error.response.data.message)
@@ -22,9 +24,11 @@ export default function UserOrders ({userId}){
     return <>
     {ordersList.map(order=>(
       <div className="border-2 m-2" key={order._id}>
+        <h2>{ dayjs(order.createdAt).format("DD/MM/YY")}</h2>
         <span>order #{order._id}</span>
         <h2>Total Price: {order.total}€</h2>
         <h3>Total products ordered: {order.products.reduce((acc,curr)=>(acc + curr.quantity),0)}</h3>
+
         {order.products.map(item=>(<div key={item._id}>
           <img src={item.product.images[0]} className="w-20"/>
           <h2>Product name: {item.product.name}</h2>
