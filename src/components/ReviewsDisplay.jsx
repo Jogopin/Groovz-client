86e535127fe1881619bb059674674e6d3d587636
jogopin <jonn.g.pineda@gmail.com>
2023-07-25 21:05:38 +0200

    Remove unused imports from ReviewsDisplay component

--- src/components/ReviewsDisplay.jsx
+++ src/components/ReviewsDisplay.jsx
@@ -1,5 +1,3 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import RatingDisplay from "./RatingDisplay";
 
 export default function ReviewsDisplay ({reviewsList, showMoreReviews, canShowMore}){
