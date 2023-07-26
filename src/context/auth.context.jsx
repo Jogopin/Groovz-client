3e5ac8a0b1f0d6d9f935b1bdb6f8a47bc4872200
jogopin <jonn.g.pineda@gmail.com>
2023-07-26 12:56:00 +0200

    Replace direct axios call with verifyUser function

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,77 +1,78 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
-import { clearTokenHeaders, setTokenInHeaders } from "../services/api";
+import {
+  clearTokenHeaders,
+  setTokenInHeaders,
+  verifyUser,
+} from "../services/api";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-function AuthProviderWrapper(props){
+function AuthProviderWrapper(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
-    const [isLoggedIn,setIsLoggedIn] = useState(false)
-    const [isLoading, setIsLoading]= useState(true)
-    const [user,setUser] = useState(null)
+  const storeToken = (token) => {
+    localStorage.setItem("authToken", token);
+    setTokenInHeaders(token);
+  };
+  const removeToken = () => {
+    localStorage.removeItem("authToken");
+    clearTokenHeaders();
+  };
 
-    const storeToken = (token)=>{
-        localStorage.setItem("authToken",token)
-        setTokenInHeaders(token)
+  const authenticateUser = async () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    if (storedToken) {
+    try {
+        const response = verifyUser();
+        //If the server verifies that the JWT token is valid
+        setUser(response);
+        setIsLoggedIn(true);
+        setIsLoading(false);
+        setTokenInHeaders(storedToken);
+      } catch (error) {
+        // If the server sends an error response (invalid token)
+        // Update state variables
+        setIsLoggedIn(false);
+        setIsLoading(false);
+        setUser(null);
+      }
+    }else{
+       // If the token is not available (or is removed)
+       setIsLoggedIn(false);
+       setIsLoading(false);
+       setUser(null);
     }
-    const removeToken =()=>{
-        localStorage.removeItem("authToken")
-        clearTokenHeaders()
-    }
-    
+  };
 
-    const authenticateUser =()=>{
+  const logOutUser = () => {
+    removeToken();
 
-        const storedToken = localStorage.getItem("authToken")
+    //update the state variables
+    authenticateUser();
+  };
 
-        if (storedToken) {
-          axios
-            .get(`${import.meta.env.VITE_API_URL}/auth/verify`, {
-              headers: { Authorization: `Bearer ${storedToken}` }
-            })
-            .then((response) => {
-              //If the server verifies that the JWT token is valid
-              setUser(response.data);
-              setIsLoggedIn(true);
-              setIsLoading(false);
-              setTokenInHeaders(storedToken)
-              
-            })
-            .catch((error) => {
-              // If the server sends an error response (invalid token)
-              // Update state variables
-              setIsLoggedIn(false);
-              setIsLoading(false);
-              setUser(null);
-            });
-        } else {
-          // If the token is not available (or is removed)
-          setIsLoggedIn(false);
-          setIsLoading(false);
-          setUser(null);
-        }
+  useEffect(() => {
+    authenticateUser();
+  }, []);
 
-        
-    }
-
-    const logOutUser =()=>{
-        
-        removeToken()
-
-        //update the state variables
-        authenticateUser()
-    }
-    
-    useEffect(()=>{
-        authenticateUser()
-    },[])
-
-        return(
-        <AuthContext.Provider value={{isLoggedIn,isLoading,user,storeToken,authenticateUser,logOutUser }}>
-            {props.children}
-        </AuthContext.Provider>
-
-    )
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        isLoading,
+        user,
+        storeToken,
+        authenticateUser,
+        logOutUser,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
 
-export { AuthProviderWrapper, AuthContext }
+export { AuthProviderWrapper, AuthContext };
