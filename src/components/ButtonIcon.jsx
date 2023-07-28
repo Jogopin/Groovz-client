4434aafad83b59898acbc17690b080a7fd377f73
jogopin <jonn.g.pineda@gmail.com>
2023-07-28 18:25:46 +0200

    Add new ButtonIcon component

---
+++ src/components/ButtonIcon.jsx
@@ -0,0 +1,13 @@
+export default function ButtonIcon({ handleClick, iconImage, alt, variant }) {
+    const buttonStyle = variant === "secondary" ? "bg-zinc-400" : "bg-red-700";
+  
+    return (
+      <button
+        onClick={handleClick}
+        className={`${buttonStyle} h-8 w-8 rounded-md p-0.5 duration-200 ease-in hover:scale-110`}
+      >
+        <img className="invert" src={iconImage} alt={alt} />
+      </button>
+    );
+  }
+  
