d46a6363eae00a3a67079c5b4f1ecb41366e9299
jogopin <jonn.g.pineda@gmail.com>
2023-07-28 18:27:26 +0200

    Refactor ProfileDetails component Refactor the ProfileDetails component to use the EditableUserInfo component for editing user personal details. Update styling and layout of the component for better UI/UX. Use new ButtonIcon components for the edit, save, and cancel actions.

--- src/pages/ProfilePage/ProfileDetails.jsx
+++ src/pages/ProfilePage/ProfileDetails.jsx
@@ -1,31 +1,34 @@
 import { useEffect, useState } from "react";
 import { updateUserDetails } from "../../services/api";
 import useUser from "../../hooks/useUser";
-import InputLabel from "../../components/InputLabel";
+import { doneIcon, editIcon, xIcon } from "../../assets/icons";
+import EditableUserInfo from "../../components/EditableUserInfo";
+import ButtonIcon from "../../components/ButtonIcon";
 
 export default function ProfileDetails({ userId }) {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [address, setAddress] = useState("");
+  const [profileUserDetails, setProfileUserDetails] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+  });
   const { userDetails } = useUser(userId);
   const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
-    setFirstName(userDetails.firstName ?? "");
-    setLastName(userDetails.lastName ?? "");
-    setAddress(userDetails.address ?? "");
+    setProfileUserDetails({
+      firstName: userDetails.firstName ?? "",
+      lastName: userDetails.lastName ?? "",
+      address: userDetails.address ?? "",
+    });
   }, [userDetails]);
 
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
   const handleSaveClick = async () => {
     try {
       const userData = {
-        userId: user._id,
-        firstName,
-        lastName,
-        address,
+        userId,
+        firstName: profileUserDetails.firstName,
+        lastName: profileUserDetails.lastName,
+        address: profileUserDetails.address,
       };
       await updateUserDetails(userData);
     } catch (error) {
@@ -34,81 +37,66 @@ export default function ProfileDetails({ userId }) {
       setIsEditing(false);
     }
   };
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
   const handleCancelClick = () => {
-    setFirstName(userDetails.firstName);
-    setLastName(userDetails.lastName);
-    setAddress(userDetails.address);
     setIsEditing(false);
   };
   return (
     <>
       {/* username & email */}
-      <div className="flex flex-col rounded-md">
-        <div className="w-full rounded-md bg-zinc-700 p-2 text-zinc-50">
-          <label className="block font-medium sm:text-xs">{"Username"}</label>
-          <p className="mt-1 cursor-default px-2  py-0 text-center  font-medium  sm:text-base">
-            {" "}
+      <section className="mx-auto mb-4 flex  flex-col gap-5 rounded-lg bg-white p-6 text-zinc-800 shadow-md md:flex-row">
+        <div className="w-full">
+          <label className="block text-xs font-medium text-zinc-500">
+            {"Username"}
+          </label>
+          <p className="mt-1 w-full px-2 py-1 font-medium sm:text-base">
             {userDetails.username}
           </p>
         </div>
-        <div className="my-2 w-full  rounded-md  border-2 border-zinc-800 p-2 text-zinc-800 ">
-          <label className="block font-medium sm:text-xs">{"Email"}</label>
-          <p className="mt-1 cursor-default  px-2 py-0 font-medium  sm:text-base">
+        <div className="w-full">
+          <label className="block text-xs font-medium text-zinc-500">
+            {"Email"}
+          </label>
+          <p className="mt-1 w-full px-2 py-1 font-medium sm:text-base">
             {userDetails.email}
           </p>
         </div>
-      </div>
+      </section>
       {/* Name & Address*/}
-      <div className="my-10 flex w-5/6 flex-col gap-4 rounded-md  border-2 border-zinc-800 bg-white p-4">
-        <InputLabel
-          id={"firstName"}
-          label={"First Name"}
-          input={{
-            name: "firstName",
-            value: firstName,
-            onChange: (e) => setFirstName(e.target.value),
-            placeholder: "John",
-            disabled: !isEditing,
-          }}
+      <section className="md:3/5 mx-auto mb-4 rounded-lg bg-white p-6 text-zinc-800 shadow-md">
+        <EditableUserInfo
+          userPersonalDetails={profileUserDetails}
+          setUserPersonalDetails={setProfileUserDetails}
+          isEditing={isEditing}
         />
-        <InputLabel
-          label={"Last Name"}
-          id={"last-name"}
-          input={{
-            name: "lastName",
-            value: lastName,
-            onChange: (e) => setLastName(e.target.value),
-            placeholder: "Doe",
-            disabled: !isEditing,
-          }}
-        />
-        <InputLabel
-          label={"Shipping address"}
-          id={"address-input"}
-          input={{
-            name: "address",
-            value: address,
-            onChange: (e) => setAddress(e.target.value),
-            placeholder: "Street, City, Country",
-            disabled: !isEditing,
-          }}
-        />
-      </div>
-
-      {!isEditing ? (
-        <button onClick={handleEditClick} className="btn-secundary">
-          Edit
-        </button>
-      ) : (
-        <div className="flex gap-8">
-          <button onClick={handleSaveClick} className="btn-primary">
-            Save
-          </button>
-          <button onClick={handleCancelClick} className="btn-secundary">
-            Cancel
-          </button>
+        <div className="mt-6 flex justify-end">
+          {!isEditing ? (
+            <ButtonIcon
+              handleClick={handleEditClick}
+              iconImage={editIcon}
+              alt={"edit-icon"}
+            />
+          ) : (
+            <div className="flex gap-4">
+              <ButtonIcon
+                variant={"secondary"}
+                handleClick={handleSaveClick}
+                iconImage={doneIcon}
+                alt={"save"}
+              />
+              <ButtonIcon
+                handleClick={handleCancelClick}
+                iconImage={xIcon}
+                alt={"cancel"}
+              />
+            </div>
+          )}
         </div>
-      )}
+      </section>
     </>
   );
 }
+
