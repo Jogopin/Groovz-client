5899c7936ad4189b668f9d79be6df4113dcfab1c
jogopin <jonn.g.pineda@gmail.com>
2023-08-04 00:46:57 +0200

    Update 'NotFoundPage' to use 'className' instead of 'class'

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,8 @@
 export default function NotFoundPage() {
   return (
-    <div class="grid h-screen place-content-center bg-white px-4">
-      <h1 class="uppercase tracking-widest text-gray-500">404 | Not Found</h1>
+    <div className="grid h-screen place-content-center bg-white px-4">
+      <h1 className="uppercase tracking-widest text-gray-500">404 | Not Found</h1>
     </div>
+    
   );
 }
