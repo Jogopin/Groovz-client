85816354d3d73564a51d9fd1a6f42ddceb35ee16
jogopin <jonn.g.pineda@gmail.com>
2023-08-04 12:28:34 +0200

    style: Change layout in NotFoundPage component

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,7 @@
 export default function NotFoundPage() {
   return (
-    <div className="grid h-screen place-content-center bg-white px-4">
+    <div className="flex flex-col min-h-full min-w-full  items-center  place-self-center justify-self-center">
       <h1 className="uppercase tracking-widest text-gray-500">404 | Not Found</h1>
     </div>
-    
   );
 }
