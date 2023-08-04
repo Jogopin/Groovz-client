6a32f4e35269eb5dc841c8e08e61db2e84d462b0
jogopin <jonn.g.pineda@gmail.com>
2023-08-04 16:07:08 +0200

    Remove aspect-square class to fix image rendering on mobile

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,7 +12,7 @@ const ProductCard = ({product}) => {
       <Link
       to={`/store/${product._id}`}
         // key={product._id}
-        className="relative flex aspect-square items-center justify-center rounded-lg bg-zinc-300 duration-300 ease-in-out hover:scale-105 hover:drop-shadow-2xl overflow-hidden"
+        className="relative flex  items-center justify-center rounded-lg bg-zinc-300 duration-300 ease-in-out hover:scale-105 hover:drop-shadow-2xl overflow-hidden"
       >
         <img className="h-full" src={product.images[0]} alt="product-image"/>     
         {!inStock && <span className="absolute w-full flex items-center justify-center font-extrabold text-white text-xl bg-zinc-600/40 h-full">Out of Stock</span>}
@@ -25,6 +25,7 @@ const ProductCard = ({product}) => {
           <h3 className="text-lg font-semibold ">{product.name}</h3>
           <p className=" text-xl font-bold">{product.price}€</p>
         </div>
+       
       </div>
     </div>
   );
