83356604b8bfa4ce478bce25aabae9961be5a68b
jogopin <jonn.g.pineda@gmail.com>
2023-08-09 16:34:12 +0200

    Implement LoadingSpinner component for ProductList

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,17 @@
 import HeroBanner  from "./HeroBanner";
-import defImg from "../../assets/imgs/defImg.webp"
-import defImgRed from "../../assets/imgs/defImgRed.png"
 import FavoriteProducts from "./FavoriteProducts";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 
-export default function HomePage({productsList}){
+export default function HomePage({productsList,isLoading}){
 
   const heroProduct = productsList.filter(item=>item.isHeroProduct===true)
   const favoriteProducts = productsList.filter(item=>item.isFavorite === true)
+
+  if(isLoading) return <LoadingSpinner/> 
   return (
     <>
       <HeroBanner heroProduct={heroProduct[0]}/>
-
       <FavoriteProducts productsList={favoriteProducts} />
     </>
   );
