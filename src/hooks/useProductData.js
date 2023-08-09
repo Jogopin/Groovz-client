a8dedb121217c1058a4be1b123666d203ba40e73
jogopin <jonn.g.pineda@gmail.com>
2023-08-09 16:34:36 +0200

    Add isLoading state in useProductData

--- src/hooks/useProductData.js
+++ src/hooks/useProductData.js
@@ -5,28 +5,37 @@ export const useProductData = (productId) => {
   const [productData, setProductData] = useState(null);
   const [reviewsList, setReviewsList] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading,setIsLoading] = useState(false)
 
   const updateReviewsList = async () => {
     try {
+      setIsLoading(true);
       const reviewsArray = await getReviewsFromProduct(productId);
       setReviewsList(reviewsArray);
     } catch (error) {
-      // Errors comming from the api  are handled in the callApi function from api.js    
+      // Errors coming from the API are handled in the callApi function from api.js    
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  const fetchProductData = async () => {
+    try {
+      setIsLoading(true);
+      const productObj = await getProductData(productId);
+      setProductData(productObj);
+      await updateReviewsList();
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+      console.log(`Error getting the product "${productId}", `, error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   useEffect(() => {
-    (async () => {
-      try {
-        const productObj = await getProductData(productId);
-        setProductData(productObj);
-        await updateReviewsList();
-      } catch (error) {
-        setErrorMessage(error.response.data.message);
-        console.log(`Error getting the product "${productId}", `, error);
-      }
-    })();
+    fetchProductData();
   }, [productId]);
 
-  return { productData, reviewsList, updateReviewsList, errorMessage };
+  return { productData, reviewsList, updateReviewsList, errorMessage, isLoading };
 };
